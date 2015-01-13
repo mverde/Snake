2c79e3db120b45d8d6b23b9527d2701526845d1e
mverde <martinverde.edu@gmail.com>
2015-01-12 20:00:57 -0800

    Create SnakeObject.java

---
+++ SnakeObject.java
@@ -0,0 +1,11 @@
+/**
+ * @(#)SnakeObject.java
+ *
+ *
+ * @authors Martin Verde
+ * @version 1.00 2014/5/8
+ */
+
+
+public abstract class SnakeObject {  //superclass that all game components extend
+}
